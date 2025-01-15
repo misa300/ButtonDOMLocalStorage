d3cd13e1114c93e43eb4debeff5b93cae7dc866d
misa300 <MIsaelc17@nycstudents.net>
2025-01-15 11:53:43 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+ const clickCountDisplay = document.getElementById('click-count');
+        const header = document.getElementById('header');
+        const button = document.getElementById('click-button');
